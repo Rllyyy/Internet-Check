20a3001dc30a2340fb9e2128545fa4b02f04d306
Niklas Fischer <nikii17.06@gmail.com>
2021-05-07 12:38:42 +0200

    :sparkles:Run .exe after install (#115)+➕Added Core.System.Configuration.Install

---
+++ Internet Check/RunExeAfterInstall.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Diagnostics;
+using System.Security.Permissions;
+
+namespace Internet_Check
+{
+    [RunInstaller(true)]
+    public partial class RunExeAfterInstall : System.Configuration.Install.Installer
+    {
+        public RunExeAfterInstall()
+        {
+            InitializeComponent();
+        }
+
+        //Source: https://stackoverflow.com/questions/3172406/create-custom-action-to-start-application-and-exit-installer
+        public override void Commit(System.Collections.IDictionary savedState)
+        {
+            base.Commit(savedState);
+            System.Diagnostics.Process.Start(Context.Parameters["TARGETDIR"].ToString() + "Internet Check.exe");
+            //move temp files
+            base.Dispose();
+        }
+    }
+}
